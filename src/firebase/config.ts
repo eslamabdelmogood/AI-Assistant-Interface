dc3a70891beb708d60b1e0c8cc56cf875150b677
esraa abdelmogood <esraaabdelmogood9630@gmail.com>
2025-10-23 09:58:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2895324130-dfc21",
+  "appId": "1:73945303795:web:33a5a5e9a48f776c6a4586",
+  "apiKey": "AIzaSyAoddOHrCSA1V2I5oZfToL5Zxceo0HZtfs",
+  "authDomain": "studio-2895324130-dfc21.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "73945303795"
+};
